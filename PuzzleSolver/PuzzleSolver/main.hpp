ceebdf5142f88ed58a8d4990d81bbac4d7a7ee8d
MingYuanTina <bjteana@gmail.com>
2016-05-15 15:06:31 -0400

    Add files via upload

---
+++ PuzzleSolver/PuzzleSolver/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  PuzzleSolver
+//
+//  Created by Tina Li on 5/15/16.
+//  Copyright © 2016 Tina Li. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
